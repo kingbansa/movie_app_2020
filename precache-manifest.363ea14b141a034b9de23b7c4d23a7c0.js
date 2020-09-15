9e84b7c273b8b7007a289c6bea33ca1b5687249d
kingbansa <kingbansa@naver.com>
2020-09-15 23:24:05 +0900

    Updates

---
+++ precache-manifest.363ea14b141a034b9de23b7c4d23a7c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e830ff75f9bfa533436f8c4e5cb4ba83",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "b7849e309e5593178611",
+    "url": "/movie_app_2020/static/css/main.6f2d31eb.chunk.css"
+  },
+  {
+    "revision": "93714d96a325a5936389",
+    "url": "/movie_app_2020/static/js/2.d652056b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.d652056b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7849e309e5593178611",
+    "url": "/movie_app_2020/static/js/main.127f1cac.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
